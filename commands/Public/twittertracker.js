6a10319611eb418f0536bb51aab2357da2aea9b5
mscott2016 <yahoomarley2@gmail.com>
2022-07-09 16:24:34 -0700

    hh

--- commands/Public/twittertracker.js
+++ commands/Public/twittertracker.js
@@ -35,7 +35,8 @@ class TwittertrackCommand extends Command {
 
           // screen_name:"localcultureart"
           T.get('search/tweets', {q:"from:localcultureart  since:2022-06-20" , count: 100 }, function(err, data, response) {
-            console.log(data.entities, data.user)
+            console.log(data.entities, data.user
+                ,typeof(data), Object.size(data),   Object.keys(data).length)
 
           })
            
