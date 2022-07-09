7d056e1b48185ddf51e1c07754e806551f432ffd
mscott2016 <yahoomarley2@gmail.com>
2022-07-09 15:56:10 -0700

    nmn

--- commands/Public/twittertracker.js
+++ commands/Public/twittertracker.js
@@ -34,9 +34,9 @@ class TwittertrackCommand extends Command {
           });
 
           // screen_name:"localcultureart"
-          T.get('search/tweets', {q:"from:localcultureart  since:2022-07-09" , count: 100 }, function(err, data, response) {
+          T.get('search/tweets', {q:"from:localcultureart  since:2022-06-20" , count: 100 }, function(err, data, response) {
             console.log(data)
-            
+
           })
            
         //    const channel = message.client.channels.cache.get('935671934488830013');
