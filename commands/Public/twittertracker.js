5386c95debedcdc1c808946ef12b0012fb94471e
alexhatcs <45803329+alexhatcs@users.noreply.github.com>
2022-07-14 17:02:22 -0400

    tyupeof

--- commands/Public/twittertracker.js
+++ commands/Public/twittertracker.js
@@ -36,7 +36,7 @@ class TwittertrackCommand extends Command {
           String(message);
           console.log("Message Starts here");
           console.log(message);
-          let handleName = message.search("content:");
+          console.log(typeof(message));
           console.log(handleName);
           // screen_name:"localcultureart" fields=public_metrics
           T.get('search/tweets', {q:"from:localcultureart  since:2022-06-20" , count: 100 }, function(err, data, response) {
