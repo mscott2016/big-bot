8fe3d8fed433bfc642133c5e7570f85622cfead6
mscott2016 <yahoomarley2@gmail.com>
2025-07-28 22:36:54 -0600

    tester

--- structures/Guild.js
+++ structures/Guild.js
@@ -1,34 +1,26 @@
-const { Structures } = require('discord.js');
+const { Structures, Guild } = require('discord.js');
 const { defaultPrefix } = require('../config.js');
 
-Structures.extend('Guild', Guild => {
-    class GuildExt extends Guild {
-        constructor(...args) {
-            super(...args);
-        }
-
-        // Returns the Guild prefix
-        // <Guild>.prefix
-        get prefix() {
-            return this.get('prefix', defaultPrefix);
-        }
-
-        // The following methods are all namespaced by Guild ID.
-        // Examples:
-        // <Guild>.get('loggingChannelID', [fallback]);
-        // <Guild>.set('loggingChannelID', '383430486506340352')
-        get(key, fallback) {
-            return this.client.db.get(`${this.id}_${key}`) || fallback;
-        }
-
-        set(key, data) {
-            return this.client.db.set(`${this.id}_${key}`, data);
-        }
-
-        delete(key) {
-            return this.client.db.delete(`${this.id}_${key}`);
-        }
+// Returns the Guild prefix
+// <Guild>.prefix
+Object.defineProperty(Guild.prototype, 'prefix', {
+    get() {
+        return this.get('prefix', defaultPrefix);
     }
-
-    return GuildExt;
 });
+
+// The following methods are all namespaced by Guild ID.
+// Examples:
+// <Guild>.get('loggingChannelID', [fallback]);
+// <Guild>.set('loggingChannelID', '383430486506340352')
+Guild.prototype.get = function(key, fallback) {
+    return this.client.db.get(`${this.id}_${key}`) || fallback;
+};
+
+Guild.prototype.set = function(key, data) {
+    return this.client.db.set(`${this.id}_${key}`, data);
+};
+
+Guild.prototype.delete = function(key) {
+    return this.client.db.delete(`${this.id}_${key}`);
+};
